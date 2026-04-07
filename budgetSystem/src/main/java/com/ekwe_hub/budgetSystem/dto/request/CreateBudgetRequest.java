89ea94bbe71551887a106ba2fc5edc066b4183a2
ekwe7 <ekwea753@gmail.com>
2026-04-07 12:31:52 +0100

    Adding CreateBudgetRequest DTO

--- budgetSystem/src/main/java/com/ekwe_hub/budgetSystem/dto/request/CreateBudgetRequest.java
+++ budgetSystem/src/main/java/com/ekwe_hub/budgetSystem/dto/request/CreateBudgetRequest.java
@@ -1,4 +1,34 @@
 package com.ekwe_hub.budgetSystem.dto.request;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateBudgetRequest {
+    @NotBlank(message = "Description is required")
+    @Size(max = 255, message = "Description must not exceed 255 characters")
+    private String description;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
+    private BigDecimal amount;
+
+    @NotNull(message = "Date is required")
+    private LocalDate date;
+
+    @NotNull(message = "Budget ID is required")
+    private Long budgetId;
+
 }
